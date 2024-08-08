57b2da2036a9c1e07f77ffb7c47b0f728b23751c
AlGar <2677766@mail.ru>
2024-08-08 15:34:22 +1000

    fourth commit

--- src/main/java/org/gardar/demoproject/controller/MainController.java
+++ src/main/java/org/gardar/demoproject/controller/MainController.java
@@ -1,6 +1,7 @@
 package org.gardar.demoproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,5 +10,11 @@ public class MainController {
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home() {
         return "home.html";
+    }
+    @RequestMapping(value = "/my-home", method = RequestMethod.GET)
+    public String myHome(Model page) {
+        page.addAttribute("userName", "Katy");
+        page.addAttribute("color", "red");
+        return "my-home.html";
     }
 }
